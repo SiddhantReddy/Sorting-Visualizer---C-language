88966f84d98474ea62569e7b5df9eb645e370131
SiddhantReddy <siddhantreddy13@gmail.com>
2020-09-17 17:52:56 +0530

    First Commit

---
+++ sorting_algorithm_visuallisation.c
@@ -0,0 +1,128 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+
+#define MAX 20
+int arr[MAX];
+int i,j,size,m,n,k;
+
+void generate()
+{
+    size=MAX;
+    int i;
+    for(i=0; i<size; i++)
+    {
+        arr[i]=rand()%30+1;
+    }
+}
+
+void choices()
+{
+    printf("\n");
+    printf(" 1 : Selection Sort\n");
+    printf(" 2 : Bubble Sort\n");
+    printf(" 3 : Insertion Sort\n");
+    printf(" 4 : Exit\n");
+}
+void show()
+{
+    /*for(i=0; i<1000; i++)
+    {
+        for(j=0; j<100; j++) {}
+    }*/
+    system("cls");
+    printf("\n\n");
+    for(i=0; i<size; i++)
+    {
+        printf(" %d",arr[i]);
+        printf("\t\t");
+        for(j=0; j<arr[i]; j++)
+        {
+            printf("_");
+        }
+        printf("\n");
+    }
+}
+
+void Insertion_sort()
+{
+    int m,n,k,temp;
+    for(m=1; m<=size; m++)
+    {
+        temp=arr[m];
+        for(n=0; n<m; n++)
+        {
+            if(temp<arr[n])
+            {
+                for(k=m; k>n; k--)
+                {
+                    arr[k]=arr[k-1];//putting element in index before it
+                }
+                arr[n]=temp;
+                show();
+                break;
+            }
+        }
+
+    }
+}
+
+void selection_sort()
+{
+    int temp;
+    for(m=0; m<size; m++)
+    {
+        for(n=m+1; n<size; n++)
+        {
+            if(arr[n]<arr[m])
+            {
+                temp=arr[n];
+                arr[n]=arr[m];
+                arr[m]=temp;
+                show();
+            }
+        }
+    }
+}
+
+void Bubble_sort()
+{
+    int temp;
+    for(m=0; m<size-1; m++)
+    {
+        for(n=0; n<size-m-1; n++)
+        {
+            if(arr[n+1]<arr[n])
+            {
+                temp=arr[n];
+                arr[n]=arr[n+1];
+                arr[n+1]=temp;
+                show();
+            }
+        }
+    }
+}
+
+void main()
+{
+    int ch;
+    generate();
+    show();
+    choices();
+    scanf("%d",&ch);
+    switch(ch)
+    {
+    case 1:
+        selection_sort();
+        break;
+    case 2:
+        Bubble_sort();
+        break;
+    case 3:
+        Insertion_sort();
+        break;
+    default:
+        printf("Invalid choice.");
+        break;
+    }
+}
